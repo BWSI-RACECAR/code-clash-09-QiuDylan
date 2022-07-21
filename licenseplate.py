8aba83d6cc6ad0ede76f68548b69ec176287a447
QiuDylan <100895454+QiuDylan@users.noreply.github.com>
2022-07-21 07:12:04 -0700

    Add files via upload

---
+++ licenseplate.py
@@ -0,0 +1,60 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2022
+
+Code Clash #9 - License Plate (licenseplate.py)
+
+
+Author: Wonjun Lee
+
+Difficulty Level: 5/10
+
+Prompt: Jon was speeding on the highway with his RACECAR, and the highway camera has taken a picture 
+of the RACECAR’s number plate. However, some characters on the plate are blurry. Please write a function 
+that returns the number of possible combinations of the number plate. The number plate for his RACECAR
+consists of 7 distinct characters, starting with 3 distinct alphabets and ending with 4 distinct numbers. 
+The input will be passed as a string and any blurred characters will be written as ‘.’
+
+Test Cases: 
+Input: “ABC123.” ; Output: 7
+Input: “.ON01123” ; Output: 24
+Input: “.BC.234” ; Output: 168
+"""
+
+class Solution:
+    def licensePlate(self,str):
+        # type str: string
+        # return: int
+        
+        # TODO: Write code below to return an int with the solution to the prompt
+
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        numbers  = '1234567890'
+        dot =0 
+        count = []
+        counter = []
+        for i in range(len(str)):
+            if i in numbers:
+                counter.append(i)
+            if i in alphabet:
+                count.append(i)
+            if i == '.':
+                dot +=1 
+        if (len(alphabet) - len(count)) != 0 or (len(numbers)- len(counter)) != 0:
+            output = (len(alphabet) - len(count))* (len(numbers)- len(counter)) 
+        return output
+
+
+
+
+        pass
+
+def main():
+    string1 = input()
+    tc1 = Solution()
+    ans = tc1.licensePlate(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
